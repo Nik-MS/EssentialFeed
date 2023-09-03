2d4f01caa13c69a91d500056855ad68c1820e2de
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-09-02 20:38:09 -0400

    feat: create feeditem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nikhil Menon on 9/2/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
