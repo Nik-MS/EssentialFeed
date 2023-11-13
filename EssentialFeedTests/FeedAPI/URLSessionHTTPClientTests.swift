c6b1f7cd38fd97722b9d53edcf34bfd9aa1435c4
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-11-13 09:15:27 -0500

    test: gaurantee correct URL and HTTPMethod on GET requests

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -27,6 +27,22 @@ class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_performsGETRequestWithURL() {
+        URLProtocolStub.startInterceptingRequests()
+        let url = URL(string: "http://any-url.com")!
+        let exp = expectation(description: "Wait for request")
+        URLProtocolStub.observeRequests { request in
+            XCTAssertEqual(request.url, url)
+            XCTAssertEqual(request.httpMethod, "GET")
+            exp.fulfill()
+        }
+        
+        URLSessionHTTPClient().get(from: url) { _ in }
+        
+        wait(for: [exp], timeout: 1.0)
+        URLProtocolStub.stopInterceptingRequests()
+    }
+    
     func test_getFromURL_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "http://any-url.com")!
@@ -58,11 +74,16 @@ final class URLSessionHTTPClientTests: XCTestCase {
 
 private class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
+    private static var requestObserver: ((URLRequest) -> Void)?
     
     private struct Stub {
         let data: Data?
         let response: URLResponse?
         let error: Error?
+    }
+    
+    static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+        requestObserver = observer
     }
     
     static func stub(data: Data?, response: URLResponse?, error: Error? = nil) {
@@ -75,11 +96,13 @@ private class URLProtocolStub: URLProtocol {
     
     static func stopInterceptingRequests() {
         URLProtocol.unregisterClass(URLProtocolStub.self)
+        requestObserver = nil
         stub = nil
     }
     
     // Determines whether or not the protocol is able to handle the incoming request.
     override class func canInit(with request: URLRequest) -> Bool {
+        requestObserver?(request)
         return true // intercept ALL requests.
     }
     
