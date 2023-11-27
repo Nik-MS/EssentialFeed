faf2a8f12747b8f39a5e65c4fe53371a1ee48ea7
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-11-27 08:48:08 -0500

    refactor: add LocalFeedItem data transfer representation to decouple storage frameworks from FeedItem data models

--- EssentialFeed/FeedCache/FeedStore.swift
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -12,5 +12,22 @@ public protocol FeedStore {
     typealias InsertionCompletion = (Error?) -> Void
 
     func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    func insert(_ items: [LocalFeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+// Looks like we are duplicating code, but this struct may have different reasons to changes versus
+// the original FeedItem. By copying, we allow both FeedItem and LocalFeedItem to change at their own pace.
+// This technique is called a Data Transfer Object.
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
