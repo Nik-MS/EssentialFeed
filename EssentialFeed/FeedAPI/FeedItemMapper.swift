33351edce23bd175b11d088e30f5c256c1519cb2
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-11-27 09:08:29 -0500

    refactor: add RemoteFeedItem data transfer representation to decouple the items mapper from FeedItem data models.

--- EssentialFeed/FeedAPI/FeedItemMapper.swift
+++ EssentialFeed/FeedAPI/FeedItemMapper.swift
@@ -5,42 +5,35 @@
 //  Created by Nikhil Menon on 11/4/23.
 //
 
+
 import Foundation
+
+// This struct is the same as a FeedItem, except that
+// `imageURL` is now `image`. This is to keep decoding logic isolated.
+// RemoteFeedLoader expects a JSON property with name, `imageURL`. But what if
+// another module expects the image property to be named `profilePic`?
+
+// An internal model for the internal API module.
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
 
 internal final class FeedItemsMapper {
     private struct Root: Decodable {
-        let items: [Item]
-        
-        var feed: [FeedItem] {
-            items.map(\.item)
-        }
-    }
-
-    // This struct is the same as a FeedItem, except that
-    // `imageURL` is now `image`. This is to keep decoding logic isolated.
-    // RemoteFeedLoader expects a JSON property with name, `imageURL`. But what if
-    // another module expects the image property to be named `profilePic`?
-
-    // An internal model for the internal API module.
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-        
-        var item: FeedItem {
-            FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]
     }
     
     private static var OK_200: Int { return 200 }
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
-              }
+            throw RemoteFeedLoader.Error.invalidData
+        }
         
-        return .success(root.feed)
+        return root.items
     }
 }
