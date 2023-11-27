138690ef284cfd945613b28e11f3c221a6406b60
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-11-27 09:30:50 -0500

    refactor: remove references of “items” in favor of “images” which is a domain term used by domain experts in the specs

--- EssentialFeed/FeedCache/LocalFeedImage.swift
+++ EssentialFeed/FeedCache/LocalFeedImage.swift
@@ -1,5 +1,5 @@
 //
-//  LocalFeedItem.swift
+//  LocalFeedImage.swift
 //  EssentialFeed
 //
 //  Created by Nikhil Menon on 11/27/23.
@@ -8,18 +8,18 @@
 import Foundation
 
 // Looks like we are duplicating code, but this struct may have different reasons to changes versus
-// the original FeedItem. By copying, we allow both FeedItem and LocalFeedItem to change at their own pace.
+// the original FeedItem. By copying, we allow both FeedItem and LocalFeedImage to change at their own pace.
 // This technique is called a Data Transfer Object.
-public struct LocalFeedItem: Equatable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
-    public let imageURL: URL
+    public let url: URL
     
-    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+    public init(id: UUID, description: String?, location: String?, url: URL) {
         self.id = id
         self.description = description
         self.location = location
-        self.imageURL = imageURL
+        self.url = url
     }
 }
