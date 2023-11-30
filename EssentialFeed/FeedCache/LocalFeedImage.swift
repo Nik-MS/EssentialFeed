34e4e35602c0a9a979ab7b7750a94ca776bd2d64
Nikhil M <15204718+Nik-MS@users.noreply.github.com>
2023-11-30 08:58:48 -0500

    Save Cache Feed (#3)
    * test: does not delete cache upon localfeedloader creation
    
    * test: save command requests cache deletion
    
    * refactor: extract system under test creation to a factory method to protect tests against breaking changes
    
    * fix: remove Mac Catalyst support deprecation warning by setting native mac as destination
    
    * test: save command does not request cache insertion on deletion error
    
    * test: save command requests new cache insertion on successful deletion
    
    * test: save command requests cache insertion with timestamp on successful deletion
    
    * refactor: remove redundant test code
    
    * refactor: unify store helper recieved messages to guarantee order and simplify test assertions
    
    * test: save command fails on cache deletion error
    
    * test: save command fails on cache insertion error
    
    * test: save command succeeds on successful cache insertion
    
    * refactor: extract duplicate test code into a shared helper method
    
    * feat: extract FeedStore protocol from test spy helper
    
    * test: guarantee the LocalFeedLoader dose not deliver deletion error after instance has been deallocated
    
    * fix: guarantee LocalFeedLoader does not deliver insertion error after instance has been deallocated.
    
    * refactor: invert if-logic to make code paths easier to follow
    
    * refactor: extract cache insertion into a helper function to make logic inside closure callbacks easier to follow
    
    * feat: move LocalFeedLoader and FeedStore collaborator to its own file in production
    
    * refactor: move FeedStore to its own file
    
    * refactor: add SaveResult typealias to protect future breaking changes
    
    * refactor: add LocalFeedItem data transfer representation to decouple storage frameworks from FeedItem data models
    
    * refactor: simplify test setup and assertions with a factory helper method
    
    * refactor: add RemoteFeedItem data transfer representation to decouple the items mapper from FeedItem data models.
    
    * refactor: move RemoteFeedItem to its own file
    
    * refactor: move LocalFeedItem to its own file
    
    * refactor: remove references of “items” in favor of “images” which is a domain term used by domain experts in the specs

---
+++ EssentialFeed/FeedCache/LocalFeedImage.swift
@@ -0,0 +1,25 @@
+//
+//  LocalFeedImage.swift
+//  EssentialFeed
+//
+//  Created by Nikhil Menon on 11/27/23.
+//
+
+import Foundation
+
+// Looks like we are duplicating code, but this struct may have different reasons to changes versus
+// the original FeedItem. By copying, we allow both FeedItem and LocalFeedImage to change at their own pace.
+// This technique is called a Data Transfer Object.
+public struct LocalFeedImage: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let url: URL
+    
+    public init(id: UUID, description: String?, location: String?, url: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.url = url
+    }
+}
