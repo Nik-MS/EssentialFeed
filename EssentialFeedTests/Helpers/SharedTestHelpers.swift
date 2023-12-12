6f8e8e6e9ebe8024fd6d5f7b22d4ba103ac6927b
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2023-12-12 09:18:31 -0500

    refactor: extract duplicate helpers into a shared scope

---
+++ EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Nikhil Menon on 12/12/23.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 1)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
