4ba5da677fe0e301aa06a73574e7f24b6e3b7c64
Nikhil M <15204718+Nik-MS@users.noreply.github.com>
2023-12-31 14:42:46 -0500

    Core Data Feed Store Implementation (#10)
    * fix: missing feed store spec implementations
    
    * test: add empty CoreData feed store specs
    
    * `CoreDataFeedStore.retrieve()` delivers empty on empty cache
    
    * test: assert retrieve has no side effects on empty cache
    
    * feat: add `CoreDataFeedStore` data model
    
    * feat: Add ManagedCache and ManagedFeedImage model representations
    
    * feat: Load persistent container stores upon CoreDataFeedStore initialization.
    
    * feat: add private background context to perform store operations
    
    * refactor: make `storeURL` and explicit dependency so that we can inject test-specific URLs to avoid sharing state with production and other tests
    
    * fix: typo in feedstore model object
    
    * fix: make ManagedFeedImage an ordered set
    
    * feat: `CoreDataFeedStore.retrieve()` delivers found values on non-empty cache
    
    * refactor: extract model translations into helper methods within the managed models
    
    * refactor: extract `ManagedCache` fetch request logic into a helper method within the managed model class
    
    * test(CoreDataFeedStore): retrieve has no side-effects on non-empty cache
    
    * test(insert): delivers no error on empty cache
    
    * test(insert): delivers no error on non-empty cache
    
    * test(insert): overrides previously inserted cache values
    
    * test(delete): delivers no error on empty cache
    
    * test: delete livers no error on non empty cache
    
    * test(delete): empties previously inserted cache
    
    * test: proved that `CoreDataFeedStore` side-effects` run serially
    
    * refactor: extract duplicate code into a reusable helper method
    
    * chore: move `CoreDataFeedStore` files to new infrastructure folder
    
    * chore: Move `CodableFeedStore` to the new infrastructure folder
    
    * refactor: extract reusable CoreData helpers into a separate file
    
    * refactor: extract CoreData managed model classes into separate files
    
    * refactor: separate CoreData managed model classes data from helpers with extensions

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,6 +9,18 @@ import EssentialFeed
 import XCTest
 
 class CodableFeedStoreTests: XCTestCase, FeedStoreSpecs, FailableFeedStore  {
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnEmptyCache(on: sut)
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatInsertDeliversNoErrorOnNonEmptyCache(on: sut)
+    }
+    
     
     override func setUp() {
         super.setUp()
@@ -101,6 +113,12 @@ class CodableFeedStoreTests: XCTestCase, FeedStoreSpecs, FailableFeedStore  {
         let sut = makeSUT(storeURL: noDeletePermissionURL)
         
         assertThatDeleteDeliversErrorOnDeletionError(on: sut)
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        let sut = makeSUT()
+        
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
     func test_delete_hasNoSideEffectOnDeletionError() {
