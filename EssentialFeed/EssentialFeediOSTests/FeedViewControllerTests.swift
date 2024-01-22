af245e2f609419a29b047260f9cbe0be905583c1
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-01-22 09:17:49 -0500

    test: viewDidLoad (viewIsAppearing) shows loading indicator

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -15,14 +15,19 @@ class FeedViewController: UITableViewController {
     convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
         load()
+    }
+    
+    override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        refreshControl?.beginRefreshing()
     }
     
     @objc
@@ -58,11 +63,24 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 3)
     }
     
+    func test_viewDidLoad_showsLoadingIndicator() {
+        let (sut, _) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        sut.replaceRefreshControlIfNeeded()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+        
+        sut.beginAppearanceTransition(true, animated: false)
+        sut.endAppearanceTransition()
+        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
         let sut = FeedViewController(loader: loader)
+        
         trackForMemoryLeaks(loader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, loader)
@@ -79,10 +97,39 @@ private extension UIRefreshControl {
     }
 }
 
+private extension UITableViewController {
+    func replaceRefreshControlIfNeeded() {
+        let fakeRefreshControl = FakeRefreshContol()
+        
+        self.refreshControl?.allTargets.forEach { target in
+            self.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                fakeRefreshControl.addTarget(target, action: Selector($0), for: .valueChanged)
+            }
+        }
+        
+        self.refreshControl = fakeRefreshControl
+    }
+}
+
 class LoaderSpy: FeedLoader {
     private(set) var loadCallCount: Int = 0
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         loadCallCount += 1
+    }
+}
+
+private class FakeRefreshContol: UIRefreshControl {
+    
+    private var _isRefreshing: Bool = false
+    
+    override var isRefreshing: Bool { _isRefreshing }
+    
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
     }
 }
