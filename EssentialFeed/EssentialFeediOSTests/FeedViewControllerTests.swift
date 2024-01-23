394b195a776143fa88209b2ed9c3909acff30151
Nikhil M <15204718+Nik-MS@users.noreply.github.com>
2024-01-22 21:59:23 -0500

    [Image Feed UI] Load Feed Logic/UX (MVC implementation) (#15)
    * feat(prototype): add refresh control
    
    * feat(prototype): Add shimmering animation while loading image
    
    * test: Does not load feed on init (before the view is loaded)
    
    * feat: Load feed on view did load
    
    * refactor: Replace FeedViewControllerTests.LoaderSpy references in the production types with the production FeedLoader interface
    
    * test: Extract system under test (sut) creation to a factory method
    
    * feat: Load feed on pull to refresh
    
    * refactor: Extract pull to refresh simulation into a reusable extension on `UIRefreshControl`
    …freshControl`
    
    * test: viewDidLoad (viewIsAppearing) shows loading indicator
    
    * fix: initial loading indicator is only shown on first viewIsAppearing
    
    * refactor: extract view appearance simulator into helper method
    
    * test: hide loading indicator on load completion
    
    * test: pull to refresh displays loading indicator
    
    * test: hide loading indicator on pull to refresh completion
    
    * refactor: decouple tests from specific UI controls for user initiated reloads with test-specific DSL methods
    
    * refactor:  decouple tests from specific UI loading indicators with test-specific DSL method
    
    * refactor: combine relevant tests to eliminate temporal coupling bugs
    
    * feat: Move `FeedViewController` to production target
    
    * fix: add dependency to EssentialFeed in the test target and set deployment target versions
    
    * fix: CI_iOS workflow set specific destination

---
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,127 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOSTests
+//
+//  Created by Nikhil Menon on 1/19/24.
+//
+
+import XCTest
+import UIKit
+import EssentialFeed
+import EssentialFeediOS
+
+final class FeedViewControllerTests: XCTestCase {
+
+    func test_loadFeedActions_requestFeedFromLoader() {
+        let (sut, loader) = makeSUT()
+        
+        XCTAssertEqual(loader.loadCallCount, 0)
+        
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadCallCount, 1)
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadCallCount, 2)
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertEqual(loader.loadCallCount, 3)
+    }
+    
+    func test_loadingFeedIndicator_isVisibleWhileLoadingFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateAppearance()
+        XCTAssertTrue(sut.isShowingLoadingIndicator)
+        
+        loader.completeFeedLoading(at: 0)
+        XCTAssertFalse(sut.isShowingLoadingIndicator)
+        
+        sut.simulateUserInitiatedFeedReload()
+        XCTAssertTrue(sut.isShowingLoadingIndicator)
+        
+        loader.completeFeedLoading(at: 1)
+        XCTAssertFalse(sut.isShowingLoadingIndicator)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        trackForMemoryLeaks(loader, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, loader)
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+}
+
+private extension UITableViewController {
+    
+    func simulateUserInitiatedFeedReload() {
+        refreshControl?.simulatePullToRefresh()
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+    
+    func simulateAppearance() {
+        if !isViewLoaded {
+            loadViewIfNeeded()
+            replaceRefreshControlIfNeeded()
+        }
+        
+        beginAppearanceTransition(true, animated: false)
+        endAppearanceTransition()
+    }
+    
+    func replaceRefreshControlIfNeeded() {
+        let fakeRefreshControl = FakeRefreshContol()
+        
+        self.refreshControl?.allTargets.forEach { target in
+            self.refreshControl?.actions(forTarget: target, forControlEvent: .valueChanged)?.forEach {
+                fakeRefreshControl.addTarget(target, action: Selector($0), for: .valueChanged)
+            }
+        }
+        
+        self.refreshControl = fakeRefreshControl
+    }
+}
+
+class LoaderSpy: FeedLoader {
+    private var completions: [(FeedLoader.Result) -> Void] = []
+    var loadCallCount: Int { completions.count }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        completions.append(completion)
+    }
+    
+    func completeFeedLoading(at index: Int) {
+        completions[index](.success([]))
+    }
+}
+
+private class FakeRefreshContol: UIRefreshControl {
+    
+    private var _isRefreshing: Bool = false
+    
+    override var isRefreshing: Bool { _isRefreshing }
+    
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+}
