90133c1c627a8e549b36bb438f80b238360fdf8b
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-02-06 19:04:16 -0500

    chore: move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Nikhil Menon on 2/6/24.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
