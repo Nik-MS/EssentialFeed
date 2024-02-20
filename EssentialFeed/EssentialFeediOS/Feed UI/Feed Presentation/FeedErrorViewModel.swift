4024d6e2dad1395a777d40d03e4fe39f2ecc288a
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-02-20 08:33:17 -0500

    chore: move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Nikhil Menon on 2/20/24.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
