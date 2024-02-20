5eabb8b9ede02fb493a52403e9df045ddb26cef1
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-02-20 08:38:01 -0500

    refactor: Add `FeedErrorViewModel` factory helpers to clarify intent at the point of use

--- EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Feed Presentation/FeedErrorViewModel.swift
@@ -9,4 +9,10 @@ import Foundation
 
 struct FeedErrorViewModel {
     let message: String?
+    
+    static var noError = FeedErrorViewModel(message: nil)
+    
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
 }
