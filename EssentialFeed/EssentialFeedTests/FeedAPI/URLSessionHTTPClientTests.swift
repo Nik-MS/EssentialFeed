ac8472bd693b5256d7de35d145d49595228af4e3
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-03-12 09:07:07 -0400

    refactor(URLSessionHTTPClient): Remove default USLSessionHTTPClient init parameter dependency as clients should be mindful about which `URLSession` instance to use. For example, clients may decide to use the ephemeral URLSession configuration registered with a custom URLProtocolStub class. This way, we don't need to start/stop intercepting URL requests globally.

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -11,14 +11,9 @@ import XCTest
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        URLProtocolStub.startInterceptingRequests()
-    }
-    
     override func tearDown() {
         super.tearDown()
-        URLProtocolStub.stopInterceptingRequests()
+        URLProtocolStub.removeStub()
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
@@ -98,7 +93,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> HTTPClient {
-        let sut = URLSessionHTTPClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        
+        let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
@@ -184,12 +183,7 @@ private class URLProtocolStub: URLProtocol {
             stub = Stub(data: data, response: response, error: error, requestObserver: nil)
     }
     
-    static func startInterceptingRequests() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-    }
-    
-    static func stopInterceptingRequests() {
-        URLProtocol.unregisterClass(URLProtocolStub.self)
+    static func removeStub() {
         stub = nil
     }
     
