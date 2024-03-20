639053fa60f25995467cc5f443e2151f80facb4b
Nik-MS <15204718+Nik-MS@users.noreply.github.com>
2024-03-20 09:07:08 -0400

    feat: Move RemoteFeedImageDataLoader to production

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -0,0 +1,63 @@
+//
+//  RemoteFeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Nikhil Menon on 3/20/24.
+//
+
+import Foundation
+
+public final class RemoteFeedImageDataLoader {
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        var wrapped: HTTPClientTask?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+            wrapped?.cancel()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion)
+        task.wrapped = client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    task.complete(with: .success(data))
+                } else {
+                    task.complete(with: .failure(Error.invalidData))
+                }
+            case let .failure(clientError):
+                task.complete(with: .failure(clientError))
+            }
+        }
+        
+        return task
+    }
+}
